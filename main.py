6ee741162129c818ea363e134516f330ef27d893
Bloomy <bloomyxdkox@gmail.com>
2024-01-08 18:10:35 +0100

    Ostateczne zmiany

--- main.py
+++ main.py
@@ -9,20 +9,31 @@ turtle.shape(image)
 żółw = turtle.Turtle()
 żółw.hideturtle()
 żółw.penup()
+zrobione = []
 
 csv = pandas.read_csv("50_states.csv")
 lista = csv['state'].tolist()
-x = csv['x'].tolist()
-y = csv['y'].tolist()
-print(lista)
-
-stan = screen.textinput(title="Nazwa Stanu?", prompt="Jaki jest kolejny Stan?").title()
-print(stan)
-
-if stan in lista:
-    żółw.goto(172,52)
-    żółw.write(stan)
 
 
+while len(zrobione) < 50:
+    stan = screen.textinput(title=f"{len(zrobione)}/50 Nazwa Stanu?", prompt="Jaki jest kolejny Stan?").title()
 
-turtle.mainloop()
+    if stan == 'Exit':
+        braki = []
+        for stann in lista:
+            if stann not in zrobione:
+                braki.append(stann)
+        data = pandas.DataFrame(braki)
+        data.to_csv("Stany Brakujące.csv")
+        break
+
+    if stan in lista:
+        if stan not in zrobione:
+            zrobione.append(stan)
+            koordynaty = csv[csv.state == stan]
+            print(koordynaty)
+            żółw.goto(int(koordynaty.x.iloc[0]), int(koordynaty.y.iloc[0]))
+            żółw.write(koordynaty.state.item())
+
+
+
