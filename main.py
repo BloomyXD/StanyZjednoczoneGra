1569c4323cce545ccc989d67a6c5d2e950907170
Bloomy <bloomyxdkox@gmail.com>
2024-01-08 15:54:37 +0100

    Tworzenie żółwia

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Siemano kolano")
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+żółw = turtle.Turtle()
+żółw.hideturtle()
+żółw.penup()
+
+csv = pandas.read_csv("50_states.csv")
+lista = csv['state'].tolist()
+x = csv['x'].tolist()
+y = csv['y'].tolist()
+print(lista)
+
+stan = screen.textinput(title="Nazwa Stanu?", prompt="Jaki jest kolejny Stan?").title()
+print(stan)
+
+if stan in lista:
+    żółw.goto(172,52)
+    żółw.write(stan)
+
+
+
+turtle.mainloop()
